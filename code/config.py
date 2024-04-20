b503903fc6a829b82d79c6821fccd1c63a11cfe6
Shashivardhan7100 <106879288+Shashivardhan7100@users.noreply.github.com>
2024-04-20 23:40:22 +0530

    Add files via upload

---
+++ code/config.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import os
+
+root_raincityscapes = r'D:\SEM6\CS354N\project\modified_new\1.De-Raining_of_Images-main\OurModel\data'
+train_raincityscapes_path = root_raincityscapes
+#os.path.join(root_raincityscapes, 'train')
+#train_raincityscapes_gt_path = os.path.join(root_raincityscapes, 'image')
+
+test_raincityscapes_path = root_raincityscapes
+#os.path.join(root_raincityscapes, 'test')
+#root_raincityscapes
+#os.path.join(root_raincityscapes, 'test')
+#test_raincityscapes_gt_path = os.path.join(root_raincityscapes, 'image')
+
